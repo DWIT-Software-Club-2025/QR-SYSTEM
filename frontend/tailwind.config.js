08cdea8005820220191c9d3e985215cd8441cb85
T-Poison10 <dipansilwal10@gmail.com>
2024-09-13 16:55:47 +0545

    added_Styles

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,14 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkShade: "#011716",
+        lightShade: "#06C2B3",
+        textShade: "#DADADA"
+      }
+    }
   },
   plugins: [],
 }
